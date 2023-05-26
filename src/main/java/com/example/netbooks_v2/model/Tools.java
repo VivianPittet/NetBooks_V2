844b44cc7dad8b34bfc8de363cf29839978e13a2
CandiceDeleze <candicedeleze@hes-so.ch>
2023-05-26 09:33:10 +0200

    Modification signature Tool

--- src/main/java/com/example/netbooks_v2/model/Tools.java
+++ src/main/java/com/example/netbooks_v2/model/Tools.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 
 public interface Tools
 {
-    public String SearchName();
-    public  ArrayList<Book> SearchWriter();
-    public ArrayList<Book> SearchType();
-    public ArrayList<String> sortByName();
-    public ArrayList<String> sortByWriter();
-    public ArrayList<String> sortByType();
+    public String SearchName(String BookName, ArrayList<Book> booklib);
+    public  ArrayList<Book> SearchWriter(String writer, ArrayList<Book> booklib);
+    public ArrayList<Book> SearchType(String type, ArrayList<Book> booklib);
+    public ArrayList<String> sortByName(ArrayList<Book>booklib);
+    public ArrayList<String> sortByWriter(ArrayList<Book> booklib);
+    public ArrayList<String> sortByType(ArrayList<Book> booklib);
 
    // public void SortBook();
 }
