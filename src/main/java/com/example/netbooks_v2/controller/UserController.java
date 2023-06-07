dd315d55a9f5d75d664b1a4cf669ba8fe03f5919
LikouNej <erol88@hotmail.ch>
2023-06-07 13:39:48 +0200

    User_View

--- src/main/java/com/example/netbooks_v2/controller/UserController.java
+++ src/main/java/com/example/netbooks_v2/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.netbooks_v2.controller;
 import com.example.netbooks_v2.HelloApplication;
 import com.example.netbooks_v2.model.Book;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -10,56 +11,112 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.Writer;
+import java.util.*;
 
-public class UserController {
+public class UserController  {
     @FXML
     private Label welcomeText;
 
+    // Id for search method
+    @FXML
+    private ImageView SearchPicture;
+    @FXML
+    private ListView<String> bListView = new ListView<>();
 
-
+    // Id for sort method
     @FXML
     private TextField SearchBar;
     //@FXML
    // private ImageView SearchPicture;
-   @FXML
-   private ChoiceBox<String> SearchW;
+  // @FXML
+  // private ChoiceBox<String> SearchW;
     @FXML
-    private Label bWriter = new Label();
+    private Label  Name= new Label();
+    @FXML
+    private Label Writer = new Label();
+    @FXML
+    private Label Type = new Label();
+    @FXML
+   private ArrayList<Book> TabSearch = HelloApplication.LibraryTest1.getBookLibrary();
+   // @FXML
+   //private Map<Article>, Integer> articles;
+
+       // this.articles = new HashMap<Article, Integer>();
+    @FXML
+    private Book bookN;
 
 
-   public static void btnsarchBookType(ArrayList bListView){
-       //stage mainWindow=(stage)Search.getScene();
 
-
-    }
-    public static void btnsarchBookName(ArrayList bListView){
-       // stage mainWindow=(stage)Search.getScene();
-
-
-    }
-    public static void btnsarchBookWriter(ArrayList bListView){
-        //stage mainWindow=(stage)Search.getScene();
-
-
-    }
-    public static void btnsortBookWriter(ArrayList<String> bListView){
-        //stage mainWindow=(stage)Search.getScene();
-        ArrayList<String > ListView = new ArrayList<String>();
-     ListView.addAll(bListView);
-    //sort
-        for(int i=0; i<ListView.size();i++)
-            if (bListView.get(i).equals(bListView))
-
-                for(i = 0; i < ListView.size(); i++)
-                System.out.println( ListView.get(i).toString() + "\n");
-
-
+//Méthode pour effacer la recherche
+    @FXML
+    protected void SearchChoiceSelected() {
+        SearchBar.setDisable(false);
+        SearchPicture.setVisible(false);
+        bListView.getItems().clear();
+        Name.setText("");
+        Writer.setText("");
+        Type.setText("");
 
 
     }
-    public static void btnsortBookName(ArrayList bListView){
+
+
+// Methode pour chercher des livres
+
+
+
+
+
+ //Methode pour trier les livres
+
+    public static void onSortButtonClickWriter(){
+        List<String> nbookname = new ArrayList<>();
+        //TabSearch = new ArrayList<~>();
+        ArrayList<Book> TabSearch=  HelloApplication.LibraryTest1.getBookLibrary();
+        String temp;
+        Book bookn=
+
+
+
+
+        for (int i=0;i<TabSearch.size();i++){
+            while (Book)
+            Collections.sort(TabSearch);
+
+
+           // for (int j=i+1;j<TabSearch.size();j++){
+                //if(TabSearch.get(i).compareTo(TabSearch.get(j))<0){
+           /* if( TabSearch[i])
+                   nbookname =TabSearch.get(i);
+                    TabSearch.set(i,TabSearch.get(j));
+                    TabSearch.set(j,nbookname);*/
+
+        //        }
+      //      }
+        }
+    System.out.println("liste par ordre Alphabetique"+TabSearch);
+
+    }
+    public static void onSortButtonClickName(){
+        String temp;
+        List<String> TabSearch= new ArrayList<String>();
+
+        /*for (int i=0;i<TabSearch.size();i++){
+            for (int j=i+1;j<TabSearch.size();j++){
+                if(TabSearch.get(i).compareTo(TabSearch.get(j))<0){
+                    temp=TabSearch.get(i);
+                    TabSearch.set(i,TabSearch.get(j));
+                    TabSearch.set(j,temp);
+
+                }
+            }
+        }*/
+        Collections.sort(TabSearch);
+        System.out.println("la liste apès tris : " + TabSearch);
+
+    }
+   /* public static void btnsortBookName(ArrayList bListView){
         //stage mainWindow=(stage)Search.getScene();
 
 
@@ -68,7 +125,38 @@ public class UserController {
         //stage mainWindow=(stage)Search.getScene();
 
 
+    }*/
+
+    //methode de creation d'une pannier achat
+   // public static void  Panier {
+
+
+
+        public static void ajouterArticle(Book book, int qte) {
+            Integer quantity = ajouterArticle().get(book);
+            if (quantity == null) {
+                quantity = Integer.valueOf(0);
+
+            }
+            quantity = Integer.valueOf(quantity.intValue() + 1);
+            ajouterArticle().put(book, quantity);
+        }
+
+        public static void supprimerArticle(Book book) {
+            supprimerArticle().remove(book);
+        }
+
+        public int calculerPanier() {
+            int total = 0;
+            Iterator<Entry<Book, Integer>> il = this.articles.entrySet().iterator();
+            while (il.hasNext()) {
+                Entry<Article, Integer> entry = il.next();
+                total += entry.getKey().getPrix() * entry.getValue().intValue();
+            }
+            return total;
+        }
     }
+
 
 
 
