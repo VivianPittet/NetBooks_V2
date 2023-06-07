f0e1315030dd7267d9736d35b0e64d10f813a0bc
VivianPittet <vivian.pittet@outlook.com>
2023-06-07 21:39:50 +0200

    Implement log in method and switching scene + correcting somes bugs in modfiy method

--- src/main/java/com/example/netbooks_v2/HelloApplication.java
+++ src/main/java/com/example/netbooks_v2/HelloApplication.java
@@ -5,7 +5,9 @@ import com.example.netbooks_v2.model.Book;
 import com.example.netbooks_v2.model.Library;
 import com.example.netbooks_v2.model.Users;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -18,10 +20,18 @@ public class HelloApplication extends Application {
     public static Admin Himen;
     public static Users Fred;
     public static PersonList allPeople;
+
+    public static Scene scene;
+    public static Scene scene2;
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Admin-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 750, 500);
+        scene = new Scene(fxmlLoader.load(), 1200, 600);
+
+        FXMLLoader fxmlLoader2 = new FXMLLoader(HelloApplication.class.getResource("User-view.fxml"));
+        scene2 = new Scene(fxmlLoader2.load(), 1200, 600);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -47,6 +57,13 @@ public class HelloApplication extends Application {
         LibraryTest1.AddInCsv("aaa", "bbb", 12,"ccc",25,"ccc","DataBooks/Resources_Data_Netbooks.csv" );
         LibraryTest1.readCSV("DataBooks/Resources_Data_Netbooks.csv");
         launch();
+    }
+    public static void switchScene (ActionEvent event){
+        Stage stage2 = new Stage();
+        ((Node)(event.getSource())).getScene().getWindow().hide();
+        stage2.setScene(scene2);
+
+        stage2.show();
     }
 }
 
