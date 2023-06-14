cf58d611ab09d268ca09233b6ab988f9c315d611
LikouNej <erol88@hotmail.ch>
2023-06-14 09:21:13 +0200

    User_View

--- src/main/java/com/example/netbooks_v2/controller/UserController.java
+++ src/main/java/com/example/netbooks_v2/controller/UserController.java
@@ -16,30 +16,58 @@ public class UserController  {
     private Label welcomeText;
 
     // Id for search method
+    ArrayList<Book> blib = HelloApplication.LibraryTest1.getBookLibrary();
+    @FXML
+    private Label bName;
+    @FXML
+    private Label bWriter;
+    @FXML
+    private Label bType;
+    @FXML
+    private Label bPages;
+    @FXML
+    private ChoiceBox<String> SearchChoice;
+    private String[] CBName = {"Name", "Writer", "Type"};
+    @FXML
+    private TextField SearchBar;
+    @FXML
+    private ImageView SearchPicture;
+    @FXML
+    private ListView<String> bListView = new ListView<>();
+  /* @FXML
+    private ScrollPane bScrollPane;
+    @FXML
+    private Button ShowBook;
+    @FXML
+    private Button backButton;*/
+    @FXML
+    private Book bookToShow;
 
 
 
     // Id for sort method
-    @FXML
-    private ImageView SearchPicture;
+   // @FXML
+   // private ImageView SearchPicture;
 
     @FXML
-    private Label  Name;
+    private Label  sort;
     @FXML
-    private Label Writer;
+    private Button Ok;
+
     @FXML
-    private Label Type;
-    @FXML
-    private ListView  showList;
+    private ListView <String> showList;
     @FXML
    private ArrayList<Book> TabSearch = HelloApplication.LibraryTest1.getBookLibrary();
 
-    private Book bookN;
+   // private Book bookN;
+   @FXML
+   private ChoiceBox<String> SChoice;
+   private String[] choice = {"Name", "Writer", "Type"};
 
 
 
 //Méthode pour effacer la recherche
-    @FXML
+   /* @FXML
     protected void SearchChoiceSelected() {
 
         SearchPicture.setVisible(false);
@@ -48,41 +76,150 @@ public class UserController  {
         Type.setText("");
 
 
+    }*/
+    @FXML
+    protected void resetSearchPage() {
+       // pathImageToAdd="";
+        //ShowBook.setVisible(false);
+       // bModify.setVisible(false);
+        SearchBar.setDisable(false);
+       // bScrollPane.setVisible(false);
+        bName.setText("");
+        bWriter.setText("");
+        bType.setText("");
+        bPages.setText("");
+        SearchPicture.setVisible(false);
+        bListView.getItems().clear();
+
     }
 
 
 // Methode pour chercher des livres
+@FXML
+protected void onSearchButtonClick(){
+    resetSearchPage();
+    bListView.getItems().clear(); // Clear the list, else the books appear many times
 
+    switch(SearchChoice.getValue()) {
+        case "Name":
+            String pathimage = "not the good path";
+            bookToShow = HelloApplication.Vivian.SearchName(SearchBar.getText(), HelloApplication.LibraryTest1.getBookLibrary()); // Récupère champs de texte et renvoi le livre correspondant.
+            if (bookToShow != null) {
+                pathimage = bookToShow.getImagePath();
+                bName.setText("Name: " + bookToShow.getName());
+                bWriter.setText("Writer: " + bookToShow.getWriter());
+                bType.setText("Type: " + bookToShow.getType());
+                bPages.setText("Pages: " + Integer.toString(bookToShow.getPages()));
+                try {
+                    Image img = new Image("file:" + pathimage);
+                    SearchPicture.setVisible(true);
+                    SearchPicture.setImage(img);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+               // bModify.setVisible(true);
+            }
+            else{
+                bName.setText("No book found");
+            }
 
-    public static void btnsortBookName(ArrayList bListView){
-        //stage mainWindow=(stage)Search.getScene();
+            break;
+        case "Writer":
+            ArrayList<Book> searchedWriter = HelloApplication.Vivian.SearchWriter(SearchBar.getText(),HelloApplication.LibraryTest1.getBookLibrary());
+            if (searchedWriter.size()>0){
+               //bScrollPane.setVisible(true);
+                ArrayList<String>bookName= new ArrayList<>();
+                for(Book b: searchedWriter){
+                    bookName.add(b.getName());
+                }
+                bListView.getItems().addAll(bookName);
+               // ShowBook.setVisible(true);
+            }
+            else{
+                bName.setText("No writer found");
 
+            }
+            break;
+        case "Type":
+            ArrayList<Book> searchedType = HelloApplication.Vivian.SearchType(SearchBar.getText(),HelloApplication.LibraryTest1.getBookLibrary());
+            if(searchedType.size()>0){
+               // bScrollPane.setVisible(true);
+                ArrayList<String>bookName= new ArrayList<>();
+                for(Book b: searchedType){
+                    bookName.add(b.getName());
+                }
+                //bListView.getItems().addAll(bookName);
+                bListView.getItems().addAll(bookName);
+               // ShowBook.setVisible(true);
+            }
+            else{
+                bName.setText("No type found");}
 
+            break;
+        default:
+            bName.setText("Please select a search method");
+            break;
     }
-    public static void btnsortBookType(ArrayList bListView){
-        //stage mainWindow=(stage)Search.getScene();
 
-
-    }
+}
 
 
 
  //Methode pour trier les livres
+ @FXML
 
-    protected void onSortButtonClickWriter(){
+    protected void onSortButtonClick(){
+
+
+        ArrayList<Book> TabSearch=  HelloApplication.LibraryTest1.getBookLibrary();  // book library
 
         ArrayList<String> nbookname = HelloApplication.Fred.sortByName(TabSearch); //List of book sort by name
 
-        ArrayList<Book> TabSearch=  HelloApplication.LibraryTest1.getBookLibrary();  // book library
-        //String temp;
-       if(nbookname!=null){
+        ArrayList<String> nbookType = HelloApplication.Fred.sortByType(TabSearch);
+
+        ArrayList<String> nbookWriter = HelloApplication.Fred.sortByWriter(TabSearch);
+
+        showList.getItems().clear();
+        switch(SChoice.getValue()){
+            case "Name":
+                for(int i = 0; i < nbookname.size(); i++)
+                    System.out.println( nbookname.get(i).toString() + "\n");
+               showList.getItems().addAll(nbookname);
+
+                break;
+
+            case "Writer":
+                for(int i = 0; i < nbookWriter.size(); i++)
+                    System.out.println("After Sorting: "+ nbookname);
+                   // System.out.println( nbookWriter.get(i).toString() + "\n");
+                     //showList.getItems().addAll(nbookWriter);
+                break;
+
+            case "Type":
+                for(int i = 0; i < nbookType.size(); i++)
+                    System.out.println("After Sorting: "+ nbookWriter);
+                    //System.out.println( nbookType.get(i).toString() + "\n");
+                showList.getItems().addAll(nbookType);
+                break;
+
+            default:
+                sort.setText("Please select a sort method");
+                break;
+
+
+        }
+
+       /*if(nbookname!=null){
         for (int i=0;i<nbookname.size();i++){
-           Collections.sort(nbookname);
+          // Collections.sort(nbookname);
            System.out.println("la liste trier"+nbookname);
+           showList.getItems();
 
            Name.setText("Name: "+ showList.getName());
            Writer.setText("Writer: " + showList.getWriter());
-           Type.setText("Type: " + showList.getType());
+           Type.setText("Type: " + showList.getType());*/
+
+
 
 
 
@@ -96,11 +233,11 @@ public class UserController  {
 
         //        }
       //      }
-        }
 
 
-    }
-    public static void onSortButtonClickName(){
+
+
+   /* public static void onSortButtonClickName(){
         ArrayList<Book> temp= new ArrayList<>();
         ArrayList<Book> TabSearch=  HelloApplication.LibraryTest1.getBookLibrary();
 
